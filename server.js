4bd21d1a677b8aae06b7fdb1d70db74ae777a9f8
Zack White <zhwhite18@yahoo.com>
2020-09-22 16:47:09 -0500

    added express.static

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 8081;
 
-//let notes = [];
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
