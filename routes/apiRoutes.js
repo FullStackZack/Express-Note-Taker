2bd91944c866fc6ce8a7f4504862273a98d4c308
Zack White <zhwhite18@yahoo.com>
2020-09-22 16:39:03 -0500

    working on apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,29 @@
 const db = require("../Develop/db/db.json")
 
 const fs = require("fs");
+
+module.exports = function(app) {
+
+    app.get("/api/notes", function(req, res) {
+
+        res.json(db);
+    });
+
+    app.post("/api/notes", function(req, res){
+
+        let newNote = req.body
+        newNote.id = uniqid()
+
+        return (fs.readFile((path.join(__dirname,"../Develop/db/db.json")), function (err, data){
+            if (err) throw err;
+
+            let notes = JSON.parse(data)
+            notes.push(newNote)
+
+            fs.writeFile((path.join(__dirname,"../Develop/db/db.json")), JSON.stringify(notes), (err) => {
+                if (err) throw err;
+                console.log("Success!")
+            })
+        }))
+    });
+}
