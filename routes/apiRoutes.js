dc305ad0cf553411812798c170c75e8f854daa4a
Zack White <zhwhite18@yahoo.com>
2020-09-24 17:37:41 -0500

    make it better

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,25 @@
-let db = require("../db/db.json")
 const uniqid = require('uniqid');
 const fs = require("fs");
 const path = require("path");
+
+const db = require("../db/db.json")
+
+//let notes = [];
 
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
 
-        res.json(db);
+        return res.json(db);
 
     });
-
-    app.get("/api/notes/:id", function(req, res) {
-
-        res.json(db[Number(req.params.id)]);
-    })
 
     app.post("/api/notes", function(req, res){
 
         let newNote = req.body
         newNote.id = uniqid()
+
+        console.log("Note ID: " + newNote.id)
 
         return (fs.readFile((path.join(__dirname,"../db/db.json")), function (err, data){
             if (err) throw err;
@@ -27,15 +27,20 @@ module.exports = function(app) {
             let notes = JSON.parse(data)
             notes.push(newNote)
 
-            fs.writeFileSync((path.join(__dirname,"../db/db.json")), JSON.stringify(notes), (err) => {
+            fs.writeFile((path.join(__dirname,"../db/db.json")), JSON.stringify(notes), (err) => {
                 if (err) throw err;
                 console.log("Success!")
-            })
-        }))
+            });
+
+            res.send(newNote);
+
+        }));
     });
 
     app.delete("/api/notes/:id", function(req, res) {
 
-
-    })
+        const noteId = req.params.id;
+        console.log(noteId);
+        
+    });
 };
